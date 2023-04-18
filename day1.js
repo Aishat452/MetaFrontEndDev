7dbcd6444f5d0c360c038663ed21a1eba7789796
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-04-18 13:32:32 +0100

    Create day1.js

---
+++ day1.js
@@ -0,0 +1,16 @@
+// Using the modulus operator, %, to test if a given number is odd
+
+
+let num1 = 2, num2 = 5;
+​
+let test1 = num1 % 2;
+​
+let test2 = num2 % 2;
+​
+let result1 = test1 == 0;
+​
+let result2 = test2 == 0;
+​
+console.log('Is', num1,'an even number?', result1);
+​
+console.log('Is', num2,"an even number?", result2);
