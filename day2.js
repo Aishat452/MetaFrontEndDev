355db4baf346db47d567546c8985665b05127778
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-04-18 13:39:08 +0100

    Logical Operators

--- day2.js
+++ day2.js
@@ -1,5 +1,13 @@
-// Using the logical || operator
+// Using the logical && operator
 
 var score = 8;
 
 console.log("Mid-level skills:", score > 0 && score < 10);
+
+// Using the logical || operator
+
+let timeRemaining = 0;
+
+let energy = 10;
+
+console.log('Game over: ', timeRemaining == 0 || energy == 0);
