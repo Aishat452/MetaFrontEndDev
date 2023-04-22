350f3a32c083f70ba3cd1fcdaa08aea6ca45b360
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-04-22 01:19:39 +0100

    Create day4.js

---
+++ day4.js
@@ -0,0 +1,29 @@
+// The for and the while loop tasks
+
+for (let i = 1; i <= 5; i++){
+
+   console.log(i);
+
+};
+
+console.log("Counting has been completed!");
+
+for (let i = 5; i > 0; i--){
+
+   console.log(i);
+
+};
+
+console.log('Countdown done');
+
+
+// the while loop
+
+while (i < 6){
+
+   console.log(i);
+   i++;
+
+};
+
+console.log("counting completed");
